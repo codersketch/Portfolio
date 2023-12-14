688683b6919563574301770fb987c08520b69931
Codersketch <akashhalder277@gmail.com>
2023-12-14 16:42:11 +0530

    update

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        silkscreen: ["Silkscreen", "sans-sarif"],
+        roboto: ["Roboto", "sans-sarif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
